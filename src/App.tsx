17c48e43b4e12df4fafc1815b6394111e6701a07
Benjamin Manjolo <benjaminmanjolo39@gmail.com>
2026-01-08 21:32:38 +0200

    first lesson setting up typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const name: string = "Benjamin";
+  const age: number = 25;
+  const isDeveloper: boolean = true;
+
+  // Try breaking it on purpose (commented out):
+  // name = 123; // Error: Type 'number' is not assignable to type 'string'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello {name}!</h1>
+      <p>Age: {age}</p>
+      <p>Developer: {isDeveloper ? "Yes" : "No"}</p>
     </div>
   );
 }
